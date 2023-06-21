6eb1c380ee20df0a0d3f4310deba4c2aa1f7c141
yoshioka <ky19991008@gmail.com>
2023-06-21 16:04:02 +0900

    Search TA

--- app/controllers/assign_shift_controller.rb
+++ app/controllers/assign_shift_controller.rb
@@ -2,4 +2,11 @@ class AssignShiftController < ApplicationController
   def index
   @course = Course.find(params[:course_id])
   end
+
+  def search
+    number = params[:number]
+    name = params[:name]
+    @search_result = TeachingAssistant.where("name LIKE ? AND number LIKE ?", "%#{name}%","%#{number}%")
+    render json: @search_result
+  end
 end
