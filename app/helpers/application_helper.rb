2efc62cac7aef275ee0e7743324f2bbb30603505
Shunpei Nakayama <pb8y7087@s.okayama-u.ac.jp>
2023-07-10 15:40:14 +0900

    add year designation function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,24 @@ module ApplicationHelper
       time_allocation += each_time_allocation
 
     end
-
-
-
       time_allocation
+  end
+
+
+  def current_fiscal_year
+    # 現在の日付を取得
+    current_date = Date.today
+
+    # 年度の開始日を設定
+    fiscal_year_start = Date.new(current_date.year, 4, 1)
+
+    # ログインした日付が年度開始日より前の場合、前の年度を取得
+    if current_date < fiscal_year_start
+      fiscal_year = current_date.year - 1
+    else
+      fiscal_year = current_date.year
+    end
+
+    fiscal_year
   end
 end
