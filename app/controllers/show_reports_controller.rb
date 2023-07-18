bb4b5a50da3e301d0718fd4ea5f12b2e58c4a2db
Toru Nakamichi <56828940+toruko44@users.noreply.github.com>
2023-07-18 14:08:07 +0900

    download and view

--- app/controllers/show_reports_controller.rb
+++ app/controllers/show_reports_controller.rb
@@ -5,6 +5,7 @@ class ShowReportsController < ApplicationController
   def index
   end
 
+
   def search
     if params[:word_number].present? && params[:word_number].length < 128
       @word = params[:word_number]
@@ -24,6 +25,7 @@ class ShowReportsController < ApplicationController
 
     render 'show_reports/index'
   end
+
   
   def search_month(month,day,work_info,worksheet)
     case month
@@ -53,6 +55,8 @@ class ShowReportsController < ApplicationController
       search_day(day,46,21,work_info,worksheet)
     end
   end
+
+
   def search_day(day,row,line,work_info,worksheet)
     for j in 0..4 do
       for i in 0..6 do
@@ -66,9 +70,49 @@ class ShowReportsController < ApplicationController
       end
     end
   end
+
+
+  def view_info1
+    id = params[:student_id]
+    @teaching_assistant = TeachingAssistant.find_by("number LIKE ?", "#{id}")
+    @shifts = Assignment.where("teaching_assistant_id LIKE ?", "#{@teaching_assistant.id}")
+    @course_info = {}
+    @work_infos = {}
+    @shifts.each_with_index do |shift, index|
+      course_info = Course.find_by("id LIKE ?", "#{shift.course_id}")
+      work_infos = WorkHour.where("assignment_id LIKE ?", "#{shift.id}")
+      @course_info[shift.id] = course_info  
+      @work_infos[shift.id] = work_infos  
+    end
+    render 'show_reports/index'
+  end
+
+
+  def view_info2
+    id = params[:student_id]
+    @teaching_assistant = TeachingAssistant.find_by("number LIKE ?", "#{id}")
+    @shifts = Assignment.where("teaching_assistant_id LIKE ?", "#{@teaching_assistant.id}")
+    @work_infos = {}
+    @month = {}
+    @day = {}
+    @shifts.each_with_index do |shift, index|
+      work_infos = WorkHour.where("assignment_id LIKE ?", "#{shift.id}") 
+      work_infos.each_with_index do |work_info, index2|
+        month = work_info.start_time.month
+        day = work_info.start_time.day
+        @work_infos[shift.id] = work_infos 
+        @month[shift.id] = month  
+        @day[shift.id] = day  
+      end
+    end
+    render 'show_reports/index'
+    # redirect_to show_reports_index_path
+  end
+
+
   def write_excel1
-    @id = params[:student_id]
-    teaching_assistant = TeachingAssistant.find_by("number LIKE ?", "#{@id}")
+    id = params[:student_id]
+    teaching_assistant = TeachingAssistant.find_by("number LIKE ?", "#{id}")
     shifts = Assignment.where("teaching_assistant_id LIKE ?", "#{teaching_assistant.id}")
     workbook = RubyXL::Parser.parse('app/assets/excel/report_type1.xlsx')
     worksheet = workbook[1]
@@ -79,7 +123,6 @@ class ShowReportsController < ApplicationController
     shifts.each_with_index do |shift, index|
       course_info = Course.find_by("id LIKE ?", "#{shift.course_id}")
       work_infos = WorkHour.where("assignment_id LIKE ?", "#{shift.id}")
-    
       work_infos.each_with_index do |work_info, index2|
         worksheet.add_cell(36 + count, 1, course_info.number.to_s.force_encoding("UTF-8"))
         worksheet.add_cell(36 + count, 3, course_info.name.to_s.force_encoding("UTF-8"))
@@ -92,14 +135,15 @@ class ShowReportsController < ApplicationController
     end
     workbook.write('app/assets/excel/write_report1.xlsx')
     session[:file_path] = 'app/assets/excel/write_report1.xlsx'
-    redirect_to download_excel_path
+    redirect_to download_excel_path 
   end
+
+  
   def write_excel2
     id = params[:student_id]
     teaching_assistant = TeachingAssistant.find_by("number LIKE ?", "#{id}")
     shifts = Assignment.where("teaching_assistant_id LIKE ?", "#{teaching_assistant.id}")
     workbook = RubyXL::Parser.parse('app/assets/excel/report_type2.xlsx')
-    
     worksheet = workbook[0]
     worksheet.add_cell(2, 4, teaching_assistant.name)
     worksheet.add_cell(2, 18, teaching_assistant.number)
@@ -115,11 +159,12 @@ class ShowReportsController < ApplicationController
     session[:file_path] = 'app/assets/excel/write_report2.xlsx'  
     redirect_to download_excel_path
   end
+
+
   def download_excel
     file_path = session[:file_path]
     send_file file_path, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', disposition: 'attachment'
   end
-
 
 
 end
